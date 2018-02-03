9aa249f17c0b925642c55b038c169c18f806c9f0
talexey <tatyana.alexeyev@searshc.com>
2018-02-02 16:37:10 -0800

    added the new file

--- src/main/scala/MultipleTraitsExtension.scala
+++ src/main/scala/MultipleTraitsExtension.scala
@@ -1,37 +1,41 @@
 
-
-
 trait trait1 {
-   var id = 0;
-  def move()={println ("this is trait 1")};
+   var id = 0
+  def move()={println ("this is trait 1")}
    }
 
 
- trait t2 extends trait1 {
-   override def move()={println ("this is trait 2")};
+ trait trait2 extends trait1 {
+   override def move()={println ("this is trait 2")}
  }
 
-trait t3  extends t2{
+trait trait3 extends trait2 {
 
-  override def move() = {println ("this is trait 3")};
+  override def move() = {println ("this is trait 3")}
 
 }
 
 
-class C extends  trait1  with t2 with t3 {
+class C extends  trait1  with trait2 with trait3 {
 def cdef = {println("this is class C")}
 
 }
 
+// override is needed for the class as well
+class C1 extends trait2  {
+  override def move() = {println("C1 is moving")}
 
+ }
 
-
-object MultipleTraitDemo {
+object MultipleTraitsExtension {
   def main(arg: Array[String]): Unit = {
     val c = new C
     c.move
 
+  val c1 = new C1
+      c1.move
+    }
   }
-}
+
 
 
